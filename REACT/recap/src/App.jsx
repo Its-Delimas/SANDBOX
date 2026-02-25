71e076ea8e52e8bc4902252a82ee1f7eb8f4ed89
Its-Delimas <spencerdelimas@gmail.com>
2026-02-25 10:02:56 +0300

    Form Handling in React ~ Simple

--- REACT/recap/src/App.jsx
+++ REACT/recap/src/App.jsx
@@ -1,13 +1,47 @@
-// inline rendering &&
+// Building Forms in React
+import { useState } from "react";
 
-export default function App() {
-  const NewEmail = 2;
+// import styles from "styles.module.css";
+
+export default function Form() {
+  const [username, setUsername] = useState();
+  // ~create a state to store the error messages
+  const [usernameError, setUsernameError] = useState();
+
+  // function to handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (usernameError) {
+      alert("Unable to Submit: Form Contains errors");
+    } else {
+      alert(username);
+    }
+  };
+  //perfoming form validation
+
+  const handleUsername = (e) => {
+    const { value } = e.target;
+    setUsername(value);
+    // validate username value
+    if (value.length <= 6) {
+      setUsernameError("Username length must be more than 6 characters");
+    } else {
+      setUsernameError();
+    }
+  };
 
   return (
-    <>
-      <h1>Hello There</h1>
-
-      {NewEmail > 0 && <h2>You have {NewEmail} new emails in your inbox</h2>}
-    </>
+    <form onSubmit={handleSubmit}>
+      Username:
+      <input
+        type="text"
+        name="username"
+        value={username}
+        onChange={handleUsername}
+      />
+      <p>{usernameError}</p>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
