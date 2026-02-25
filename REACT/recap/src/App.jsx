ca4291ce442e4ff6d7bf9cf8fc75ae6b3e99b840
Its-Delimas <spencerdelimas@gmail.com>
2026-02-25 10:16:03 +0300

    Network Request in React - fetch API

--- REACT/recap/src/App.jsx
+++ REACT/recap/src/App.jsx
@@ -1,47 +1,20 @@
-// Building Forms in React
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-// import styles from "styles.module.css";
+export default function App() {
+  const [title, setTitle] = useState("");
 
-export default function Form() {
-  const [username, setUsername] = useState();
-  // ~create a state to store the error messages
-  const [usernameError, setUsernameError] = useState();
-
-  // function to handle submit
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (usernameError) {
-      alert("Unable to Submit: Form Contains errors");
-    } else {
-      alert(username);
-    }
-  };
-  //perfoming form validation
-
-  const handleUsername = (e) => {
-    const { value } = e.target;
-    setUsername(value);
-    // validate username value
-    if (value.length <= 6) {
-      setUsernameError("Username length must be more than 6 characters");
-    } else {
-      setUsernameError();
-    }
+  const getData = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+    const task = await response.json();
+    console.log(task);
+    setTitle(task.title);
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      Username:
-      <input
-        type="text"
-        name="username"
-        value={username}
-        onChange={handleUsername}
-      />
-      <p>{usernameError}</p>
-      <button type="submit">Submit</button>
-    </form>
-  );
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <h1>{title}</h1>;
 }
