3608c3dea5afc93c9d8ead42e0b33e97df9d1ac7
Its-Delimas <spencerdelimas@gmail.com>
2026-05-02 22:58:06 +0300

    lesson: loops~for

--- GO/main.go
+++ GO/main.go
@@ -3,34 +3,34 @@ package main
 import (
 	"fmt"
 	"sort"
-	"strings"
 )
 
 func main() {
-	//strings package
-	greeting := "hello there ninja"
+	x := 0
+	//while loop but its for in GO
+	for x < 5 {
+		fmt.Println("value of x is :", x)
+		x += 2
+	}
+	//normal for loop but does not use parenthesis
+	for i := 0; i < 5; i += 1 {
+		fmt.Println("Odd Numbers:", i)
+	}
 
-	fmt.Println(strings.Contains(greeting, "hello"))
-	// method contains, 2 arg, 1arg is the searh body and the 2nd is the actual search, returns true or false
-	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-	//third arg is the value to relace with, does not actaully alter the string, returns the new string
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "ll"))
-	//returns the position containing the doule ll
-	fmt.Println(strings.Split(greeting, " "))
-
-	//sort package ~ changes the original slice
-	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
-	sort.Ints(ages)
-	fmt.Println(ages)
-
-	index := sort.SearchInts(ages, 30)
-	fmt.Println(index)
-
-	names := []string{"Spencer", "Delimas", "Bangoya", "Ethan", "zenira"}
+	names := []string{"Spencer", "Delimas", "Bangoya", "Luigi", "Armstrong"}
 	sort.Strings(names)
-	fmt.Println(names)
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
 
-	fmt.Println(sort.SearchStrings(names, "zenira"))
+	//for-in loop but in GO its RANGE keyword
+	//cycle through a slice
+	for index, value := range names {
+		fmt.Printf("The position at index %v is value %v \n", index, value)
+	}
 
+	//if you dont want to use the index
+	for _, value := range names {
+		fmt.Printf("The value %v \n", value)
+	}
 }
