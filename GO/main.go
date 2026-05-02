758f709062a9f1dbcc409f7f3b691126c2bb3a78
Its-Delimas <spencerdelimas@gmail.com>
2026-05-02 21:18:52 +0300

    lesson: print, println, printf,Sprintf

--- GO/main.go
+++ GO/main.go
@@ -3,9 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var age int = 20
-	var name = "Spencer"
-	secondName := "Delimas"
+	age := 35
+	name := "spencer"
 
-	fmt.Println(name, secondName, " ", age)
+	// print - same output on the same line
+	fmt.Print("hello")
+	fmt.Print("ninja")
+
+	// Println - opens new line automatically
+	fmt.Println("hello, Spencer")
+	fmt.Println("Go lore")
+	fmt.Println("my name is:", name, " and my name is: ", age)
+
+	// formatted strings `Printf`, %_ = format specifiers
+	fmt.Printf("my age is %v and my name is %v \n", age, name)  //%v = use default variable type
+	fmt.Printf("my age is %v and my name is %q \n", age, name)  //%q= quotes around strings
+	fmt.Printf("age is of type %T\n", age)                      //%T = returns type of var
+	fmt.Printf("you scored %0.2f ponts in your gcpa\n", 225.59) //%f = floats, o.2 for 2d.p
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v", age, name)
+	fmt.Printf("The saved string is : %v", str)
+
 }
