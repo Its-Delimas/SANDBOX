14423430d42c93145076135d0b687b56396569c7
Its-Delimas <spencerdelimas@gmail.com>
2026-05-03 01:03:25 +0300

    lesson: Structs and custom Types

--- GO/main.go
+++ GO/main.go
@@ -2,21 +2,7 @@ package main
 
 import "fmt"
 
-func updateName(x *string) {
-	*x = "Brave"
-
-}
 func main() {
-	name := "Chrome"
-	fmt.Println(name)
-
-	//pointers &var_name
-	fmt.Println("Memory address of the name is : ", &name)
-	n := &name //pointer value can be stored in var...&
-	fmt.Println("Memory address of n: ", &n)
-	fmt.Println("Value at memory address:", *n) //to get the exact value of the pointer, we use *
-	updateName(n)
-	fmt.Println(name)
+	myBill := newBill("spencer's bill")
+	fmt.Println(myBill)
 }
-
-// non pointer and pointer wrappers
