b6cbcf3381b81790c9c58c3e8eacb129761aff2b
Its-Delimas <spencerdelimas@gmail.com>
2026-05-02 23:35:01 +0300

    lesson: multiple return values

--- GO/main.go
+++ GO/main.go
@@ -2,35 +2,33 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
-func sayGreeting(n string) {
-	fmt.Printf("Good Morning %v \n", n)
-}
-func sayBye(n string) {
-	fmt.Printf("see you soon %v \n", n)
-}
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
 
-func circleArea(r float64) float64 {
-	return math.Pi * r * r
-}
-
-func cycleName(n []string, f func(string)) {
-	for _, v := range n {
-		f(v)
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
 	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
 }
 
 func main() {
-	sayGreeting("Spencer")
-	sayGreeting("Luigi")
-	sayBye("Mario")
+	fn1, sn1 := getInitials("Spencer Delimas")
+	fmt.Println(fn1, sn1)
 
-	cycleName([]string{"cloud", "tifa", "barret"}, sayGreeting)
-	cycleName([]string{"cloud", "tifa", "barret"}, sayBye)
+	fn2, sn2 := getInitials("Clement Ndathe")
+	fmt.Println(fn2, sn2)
 
-	a1 := circleArea(14.28)
-	fmt.Printf("Circle is %0.2f", a1)
+	fn3, sn3 := getInitials("Julien")
+	fmt.Println(fn3, sn3)
 
 }
