11c32589504a8b6fe8175d3b90923f1a5974575a
Its-Delimas <spencerdelimas@gmail.com>
2026-05-03 00:55:06 +0300

    lesson: pointers

--- GO/main.go
+++ GO/main.go
@@ -2,38 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-
-	//map format... varName := map[key type] valuetype{***}
-	menu := map[string]float64{
-		"Soup":   4.99,
-		"Pie":    7.99,
-		"salad":  6.99,
-		"Coffee": 3.55,
-	}
-
-	fmt.Println(menu)
-	fmt.Println(menu["salad"])
-
-	//looping through a map
-	for k, v := range menu {
-		fmt.Println(k, "-", v)
-	}
-	//ints as key type
-	Languages := map[int]string{
-		001000: "Node",
-		001001: "Express",
-		001002: "NextJs",
-		001003: "GO",
-		001004: "PostgreSQL",
-		001005: "MongoDB",
-		001006: "Tailwind v4",
-		001007: "React",
-	}
-	// changing value
-	Languages[001003] = "GOLANG"
-	for _, v := range Languages {
-		fmt.Println(v)
-	}
+func updateName(x *string) {
+	*x = "Brave"
 
 }
+func main() {
+	name := "Chrome"
+	fmt.Println(name)
+
+	//pointers &var_name
+	fmt.Println("Memory address of the name is : ", &name)
+	n := &name //pointer value can be stored in var...&
+	fmt.Println("Memory address of n: ", &n)
+	fmt.Println("Value at memory address:", *n) //to get the exact value of the pointer, we use *
+	updateName(n)
+	fmt.Println(name)
+}
+
+// non pointer and pointer wrappers
