f3e364ab318de746ce39d99294660cc59c5619a7
Its-Delimas <spencerdelimas@gmail.com>
2026-05-03 13:20:54 +0300

    lesson: Switch case

--- GO/main.go
+++ GO/main.go
@@ -33,7 +33,21 @@ func PromptOptions(b bill) {
 	reader := bufio.NewReader(os.Stdin)
 
 	opt, _ := getInput("Choose option (a- add, s- save bill, t- add tip): ", reader)
-	fmt.Println(opt)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name", reader)
+		price, _ := getInput("Item Price", reader)
+		fmt.Println(name, price)
+	case "t":
+		tip, _ := getInput("Enter tip amount (ksh)", reader)
+		fmt.Println(tip)
+	case "s":
+		fmt.Println("You chose s")
+	default:
+		fmt.Println("Invalid option...")
+		PromptOptions(b)
+	}
 
 }
 
