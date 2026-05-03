f6747ee96fe2518cee230fa4329700ed28aa38e5
Its-Delimas <spencerdelimas@gmail.com>
2026-05-03 13:55:51 +0300

    lesson: interfaces introduction

--- GO/main.go
+++ GO/main.go
@@ -1,89 +1,54 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"math"
 )
 
-// helper function
-func getInput(prompt string, r *bufio.Reader) (string, error) {
-	fmt.Print(prompt)
-	input, err := r.ReadString('\n')
-
-	return strings.TrimSpace(input), err
-
+type shape interface {
+	area() float64
+	circumf() float64
 }
 
-func createBill() bill {
-	reader := bufio.NewReader(os.Stdin)
-
-	// fmt.Print("Create a new bill name: ")
-	// name, _ := reader.ReadString('\n')
-	// name = strings.TrimSpace(name)
-	name, _ := getInput("create a new bill name: ", reader)
-
-	b := newBill(name)
-	fmt.Printf("Created %v bill <3\n", b.name)
-
-	return b
-}
-func PromptOptions(b bill) {
-	reader := bufio.NewReader(os.Stdin)
-
-	opt, _ := getInput("Choose option (a- add, s- save bill, t- add tip): ", reader)
-
-	switch opt {
-	case "a":
-		name, _ := getInput("Item name", reader)
-		price, _ := getInput("Item Price", reader)
-
-		p, err := strconv.ParseFloat(price, 64)
-		if err != nil {
-			fmt.Println("The price must be a number:")
-			PromptOptions(b)
-		}
-		b.addItem(name, p)
-		fmt.Println("Items added successfully")
-		fmt.Println("item name: ", name, "item price: ", price)
-		PromptOptions(b)
-	case "t":
-		tip, _ := getInput("Enter tip amount (ksh)", reader)
-
-		t, err := strconv.ParseFloat(tip, 64)
-		if err != nil {
-			fmt.Println("The tip must be a number:")
-			PromptOptions(b)
-		}
-		b.updateTip(t)
-
-		fmt.Println(tip)
-		fmt.Println("Tip added successfully: ")
-		PromptOptions(b)
-	case "s":
-		b.save()
-		fmt.Println("You saved a file - ", b.name)
-	default:
-		fmt.Println("Invalid option...")
-		PromptOptions(b)
-	}
-
+type square struct {
+	length float64
 }
 
-func (b *bill) save() {
-	data := []byte(b.format())
-	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
-	if err != nil {
-		panic(err)
+type circle struct {
+	radius float64
+}
 
-	}
-	fmt.Println("Bill is saved succesfully")
+// square method
+func (s square) area() float64 {
+	return s.length * s.length
+}
+func (s square) circumf() float64 {
+	return s.length * 4
+}
+
+// circle metods
+func (c circle) area() float64 {
+	return math.Pi * (c.radius * c.radius)
+}
+func (c circle) circumf() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func printShapeInfo(s shape) {
+	fmt.Printf("area of %T is: %0.2f", s, s.area())
+	fmt.Printf("ircumference of %T is: %0.2f \n", s, s.circumf())
 }
 
 func main() {
-	myBill := createBill()
-	PromptOptions(myBill)
+	shapes := []shape{
+		square{length: 7},
+		circle{radius: 14},
+		square{length: 21},
+		circle{radius: 4.9},
+	}
 
+	for _, v := range shapes {
+		printShapeInfo(v)
+		fmt.Println("---")
+	}
 }
